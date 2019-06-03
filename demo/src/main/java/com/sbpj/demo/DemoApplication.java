f05bbe146a00d3e63d28e700aaf98e5dac97042b
MOHEJIAYE <mohe123@foxmail.com>
2019-06-03 17:22:54 +0800

    服务治理Eureka：服务注册中心、服务提供者

--- demo/src/main/java/com/sbpj/demo/DemoApplication.java
+++ demo/src/main/java/com/sbpj/demo/DemoApplication.java
@@ -6,10 +6,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @EnableEurekaServer注解，启动一个服务注册中心提供给其他应用进行对话
+		*/
+//@EnableEurekaServer
 @RestController
 @SpringBootApplication
 public class DemoApplication {
@@ -40,15 +45,5 @@ public class DemoApplication {
 //	public String count() {
 //		countService.increment("sbpj.demo.count");
 //		return "";
-//	}
-//	@Bean
-//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//		// 目的是
-//		return args -> {
-//			System.out.println("来看看 SpringBoot 默认为我们提供的 Bean：");
-//			String[] beanNames = ctx.getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			Arrays.stream(beanNames).forEach(System.out::println);
-//		};
 //	}
 }
---
+++ eurekademo/eureka-client/src/main/java/com/sbpj/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,69 @@
+package com.sbpj.eurekaclient;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @EnableDiscoveryClient注解，激活Eureka中的DiscoveryClient实现
+ * （自动化配置，创建DiscoveryClient接口针对Eureka客户端的EurekaDiscoveryClient实例），
+ * 才能实现服务信息的输出
+ */
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    // log4j2 org.apache.logging.log4j.Logger
+    // private static Logger logger = LogManager.getLogger(EurekaClientApplication.class);
+
+    // log4j org.apache.log4j.Logger
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private Registration registration;  //服务注册
+
+    @Autowired
+    private DiscoveryClient client;	//服务发现客户端
+
+    public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+	    // 过期方法
+        // ServiceInstance instance = client.getLocalServiceInstance();
+        // logger.info("/test, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+
+        ServiceInstance instance = serviceInstance();
+        String result = "/test, host:" + instance.getHost() + ", service_id:" + instance.getServiceId();
+        logger.info(result);
+
+        return "From Service-A, " + result;
+	}
+
+	public ServiceInstance serviceInstance() {
+        List<ServiceInstance> list = client.getInstances(registration.getServiceId());
+        if (null != list && list.size() > 0) {
+            for (ServiceInstance itm : list) {
+                if (itm.getPort() == 8080)
+                    return itm;
+            }
+        }
+        return null;
+    }
+}
